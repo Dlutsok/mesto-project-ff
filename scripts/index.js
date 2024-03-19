0c480133dba26f2eee95261453c0bfdefcef86d0
Дан <dlutsok13@yandex.ru>
2024-03-19 12:21:29 +0300

    Исправление ошибок

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,30 @@
 const placesList = document.querySelector('.places__list');
 const templateElement = document.querySelector('#card-template');
 
-initialCards.forEach(function (element) {
+function createCard(item, { deleteCard }) {
     const cardElement = templateElement.content.cloneNode(true);
 
-    cardElement.querySelector('.card__image').src = element.link;
-    cardElement.querySelector('.card__title').textContent = element.name;
+    const cardImage = cardElement.querySelector('.card__image');
+    const cardTitle = cardElement.querySelector('.card__title');
+    const deleteButton = cardElement.querySelector('.card__delete-button');
 
+    cardImage.src = item.link;
+    cardImage.alt = item.name;
+    cardTitle.textContent = item.name;
+
+    deleteButton.addEventListener('click', function () {
+        const card = deleteButton.closest('.places__item');
+        card.remove();
+        deleteCard(item);
+    });
+
+    return cardElement;
+}
+
+initialCards.forEach(function (element) {
+    const cardElement = createCard(element, { deleteCard });
     placesList.append(cardElement);
 });
 
-const deleteButtons = document.querySelectorAll('.card__delete-button');
-
-deleteButtons.forEach(function (button) {
-    button.addEventListener('click', function () {
-        const card = button.closest('.places__item');
-        card.remove();
-    });
-});
+function deleteCard(item) {
+}
